a00f81017c483fa4574cc85b462e2d09ea80a0ef
Mitesh Jain <miteshjain2808@gmail.com>
2025-10-12 15:17:07 +0530

    updated project report schema

--- backend/src/models/ProjectReport.js
+++ backend/src/models/ProjectReport.js
@@ -5,7 +5,7 @@ const projectReportSchema = new mongoose.Schema(
     projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
     title: { type: String, required: true },
     description: { type: String },
-    reportPdf: { type: URL },
+    reportPdf: { type: String },
     submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     submittedAt: { type: Date, default: Date.now }
   },
